4b7ffee15ad353dd2ea2284ee636e78d23471e41
Rizal Kalam <89380544+rizalkalam@users.noreply.github.com>
2024-11-29 03:23:12 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,69 +1,55 @@
 <?php
 
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell
- */
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
 /*
 |--------------------------------------------------------------------------
-| Check If The Application Is Installed
+| Check If The Application Is Under Maintenance
 |--------------------------------------------------------------------------
 |
-| We check if the application is installed before we continue loading
-| the application. This check helps to prevent loading the framework
-| when the app is not installed or configured correctly.
+| If the application is in maintenance / demo mode via the "down" command
+| we will load this file so that any pre-rendered content can be shown
+| instead of starting the framework, which could cause an exception.
 |
 */
 
-if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
-    require __DIR__.'/../storage/framework/maintenance.php';
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
 }
 
 /*
 |--------------------------------------------------------------------------
-| Turn On The Application
+| Register The Auto Loader
 |--------------------------------------------------------------------------
 |
-| We load the Laravel application for handling incoming requests.
-| We will create a new instance of the kernel and send the request.
+| Composer provides a convenient, automatically generated class loader for
+| this application. We just need to utilize it! We'll simply require it
+| into the script here so we don't need to manually load our classes.
 |
 */
 
 require __DIR__.'/../vendor/autoload.php';
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
 /*
 |--------------------------------------------------------------------------
-| Handle The Request
+| Run The Application
 |--------------------------------------------------------------------------
 |
-| Here, we will pass the request to the Laravel application and send
-| the response back to the browser. This is the main entry point
-| for all requests to the application.
+| Once we have the application, we can handle the incoming request using
+| the application's HTTP kernel. Then, we will send the response back
+| to this client's browser, allowing them to enjoy our application.
 |
 */
 
-// Check if the request is an API request
-if (preg_match('/^\/api\//', $_SERVER['REQUEST_URI'])) {
-    // Route to API controller for handling API requests
-    $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-    $response = $kernel->handle(
-        $request = Illuminate\Http\Request::capture()
-    );
-    $response->send();
-    $kernel->terminate($request, $response);
-} else {
-    // Handle standard web request
-    $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-    $response = $kernel->handle(
-        $request = Illuminate\Http\Request::capture()
-    );
-    $response->send();
-    $kernel->terminate($request, $response);
-}
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
