a75f1af5fccfab6ec67edc505ba08f2a58aaad37
rizalkalam <rizalkalam78@gmail.com>
2024-12-06 19:14:45 +0700

    fix vercel config

--- api/index.php
+++ api/index.php
@@ -1,13 +1,25 @@
 <?php
- 
-// Load the Laravel application
-require __DIR__ . '/vendor/autoload.php';
-$app = require_once __DIR__ . '/bootstrap/app.php';
- 
-// Run the application
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-$response->send();
-$kernel->terminate($request, $response);
+
+// Debugging awal
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+try {
+    // Load the Laravel application
+    require __DIR__ . '/../vendor/autoload.php';
+    $app = require_once __DIR__ . '/../bootstrap/app.php';
+
+    // Run the application
+    $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+    $response = $kernel->handle(
+        $request = Illuminate\Http\Request::capture()
+    );
+    $response->send();
+    $kernel->terminate($request, $response);
+
+} catch (Exception $e) {
+    // Log error untuk debugging
+    error_log($e->getMessage());
+    echo "An error occurred. Check server logs.";
+}
